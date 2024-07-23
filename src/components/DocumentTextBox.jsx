75fb7c55a76d3440ceb2ca5d0c40028763a6dcd4
Hammy <68724706+hamtsu@users.noreply.github.com>
2024-07-23 12:50:13 +1000

    Added document saving and refactored document page

---
+++ src/components/DocumentTextBox.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+import Quickbar from "./Quickbar";
+
+const DocumentTextBox = ({ content, documentId }) => {
+    const [currentContent, setCurrentContent] = useState(content);
+    const currentContentRef = useRef(currentContent);
+    currentContentRef.current = currentContent;
+
+    const onChange = (e) => {
+        console.log(e.target.value);
+        setCurrentContent(e.target.value);
+    };
+
+    useEffect(() => {
+        let timeout;
+
+        const save = () => {
+            if (currentContentRef.current) {
+                const parsedData = JSON.parse(window.localStorage.getItem("documentData"));
+                const documentData = parsedData[documentId];
+                documentData.content = currentContentRef.current;
+
+                parsedData[documentId] = documentData;
+
+                window.localStorage.setItem("documentData", JSON.stringify(parsedData));
+            }
+        };
+
+        const handleKeyDown = () => {
+            clearTimeout(timeout);
+
+            // save after one second of inactivity
+            timeout = setTimeout(() => {
+                console.log('Saved document changes');
+                save();
+            }, 1000);
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            clearTimeout(timeout);
+        };
+    }, [documentId]);
+
+    return (
+        <div className="flex gap-1 w-full items-center justify-center overflow-scroll-x">
+            <textarea
+                onInput={onChange}
+                className="bg-neutral-900 shadow-md h-[1000px] w-4/6 p-3 resize-none animate-fade-in-late outline-none border border-neutral-700"
+                defaultValue={content}
+                spellCheck={true}
+            />
+            
+            <Quickbar />
+        </div>
+    );
+};
+
+export default DocumentTextBox;
--- src/pages/DocumentPage.jsx
+++ src/pages/DocumentPage.jsx
@@ -6,6 +6,7 @@ import Footer from '../components/Footer'
 import { FaTrashAlt } from 'react-icons/fa'
 import useModal from '../hooks/UseModal'
 import DocumentDeleteModal from '../components/modals/DocumentDeleteModal'
+import DocumentTextBox from '../components/DocumentTextBox'
 
 const DocumentPage = () => {
     const { documentId } = useParams()
@@ -29,20 +30,18 @@ const DocumentPage = () => {
 
     return (
         <div className='h-screen flex flex-col'>
-            <div className='flex flex-col p-5 m-5 h-full animate-fade-in'>
-                <div className='flex flex-col gap-3'>
+            <div className='flex gap-3 flex-col h-full animate-fade-in items-center'>
+
+                <div className='flex flex-col gap-3 p-5 bg-neutral-900 shadow-md w-full'>
                     <div className='flex gap-2'>
                     <Button onClick={() => navigate("/")}><FaArrowLeftLong size={30} /></Button>
                     <Button onClick={deleteDocument} className="bg-red-600 border-red-400 hover:bg-red-700 active:bg-red-800"><FaTrashAlt size={30} /></Button>
                     </div>
                     <h1>Now editing <b>{documentData.name}</b></h1>
                 </div>
-                <hr className='my-5 border-neutral-600' />
-                <div className='bg-neutral-900 rounded-md p-3 h-full animate-fade-in-late'>
-                    <p>{documentData.content}</p>
-                </div>
+
+                <DocumentTextBox documentId={documentId} content={documentData.content} />
             </div>
-            <Footer />
         </div>
     )
 }
